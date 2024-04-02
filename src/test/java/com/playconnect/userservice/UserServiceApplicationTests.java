808357fc408b45c553d2ac07aa6fdfff8bfeac49
Lachezar Mitov <lachezarspam1@gmail.com>
2024-04-02 23:00:03 +0200

    Load .env variable in test

--- src/test/java/com/playconnect/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/playconnect/userservice/UserServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.playconnect.userservice;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,6 +8,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class UserServiceApplicationTests {
+
+	@BeforeAll
+	public static void setup() {
+		System.setProperty("JWT_SECRET_KEY", "your_test_secret_key_here");
+	}
 
 	@Test
 	void contextLoads() {
